810045f3c9796702a7501872839dc6f111ebd00a
Dawid Wegner <dawidwegner@Dawids-MacBook-Pro.local>
2020-05-09 16:36:57 +0200

    Add phrases support in calculation of ratings

--- wiki_search_engine/search_result_rater.py
+++ wiki_search_engine/search_result_rater.py
@@ -1,4 +1,5 @@
 import itertools
+from collections import defaultdict
 
 import numpy as np
 
@@ -18,7 +19,7 @@ class SearchResultRater(object):
         for title_word in all_title_words:
             word_base_forms = title_words_base_forms[title_word]
             if any([word_base_form in query_base_forms for word_base_form in word_base_forms]):
-                rating += 5
+                rating += 30
         return rating
 
     def _get_content_rating(self, content):
@@ -29,10 +30,27 @@ class SearchResultRater(object):
     def _get_article_id_rating(self, article_id):
         return np.exp(-article_id * self.ID_RATING_FALL_RATE)
 
+    def _get_phrases_rating(self, text, weight_of_rating):
+        words_in_text = text.lower().split(" ")
+        words_base_forms = defaultdict(lambda: {}, self.index_storage.get_words_base_forms(words_in_text))
+        last_words_matched_count = 0
+        rating = 0.0
+        query_base_forms = set(itertools.chain(*self.query_words_base_forms.values()))
+        for word in words_in_text:
+            intersected_forms = set(words_base_forms[word]).intersection(query_base_forms)
+            if len(intersected_forms) > 0:
+                rating += last_words_matched_count * weight_of_rating
+                last_words_matched_count += 1
+            else:
+                last_words_matched_count = 0
+        return rating
+
     def rate_wiki_article(self, wiki_article):
         title_rating = self._get_title_rating(wiki_article.title)
         content_rating = self._get_content_rating(wiki_article.content)
         article_id_rating = self._get_article_id_rating(wiki_article.id)
-        return title_rating + content_rating + article_id_rating
+        title_phrases_rating = self._get_phrases_rating(wiki_article.title, weight_of_rating=100)
+        content_phrases_rating = self._get_phrases_rating(wiki_article.content, weight_of_rating=10)
+        return title_rating + content_rating + article_id_rating + title_phrases_rating + content_phrases_rating
 
 
